402a92232a07b388a945b952f8630d5e26ee3ab6
Rahul Yedida <rahul.yedida@pm.me>
2021-06-10 05:03:36 -0400

    Grouped comments by issue

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 from dotenv import load_dotenv
+from itertools import groupby
 import requests
 
 from .models import Greeting
@@ -14,4 +15,10 @@ def index(request):
     r = requests.get('https://api.github.com/repos/bhermann/DoR/issues/comments', data={
         'Authorization': 'token ' + os.getenv('TOKEN')
     })
-    return render(request, "index.html", {'issues': r.json()})
+
+    comments = r.json()
+    comments.sort(key=lambda p: p['issue_url'])
+    groups = groupby(comments, key=lambda p: p['issue_url'])
+    groups = [(key.split('/')[-1], list(data)) for key, data in groups]
+
+    return render(request, "index.html", {'groups': groups})
