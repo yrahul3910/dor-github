0b70176be0bdaa72d0b8afaabac41fac85e293c2
Rahul Yedida <rahul.yedida@pm.me>
2021-07-03 23:49:16 -0400

    Add requests cache, histogram, and CSV parsing

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,32 @@ from django.http import HttpResponse
 import os
 from dotenv import load_dotenv
 from itertools import groupby
-from statsmodels.stats.inter_rater import fleiss_kappa
+from pandas.core.algorithms import isin
+from pandas.core.indexes import multi
 import requests
+from statsmodels.stats.inter_rater import fleiss_kappa
+import requests_cache
+import matplotlib.pyplot as plt
 import pandas as pd
+import base64
+import io
+from io import StringIO
+import re
+
+FILE_REGEX = '\[[a-zA-Z0-9.-]+\]\((https://github.com/bhermann/DoR[a-zA-Z0-9/.-]*)'
+
+
+def normalize_index(x):
+    if not isinstance(x, str):
+        x = str(x)
+                                
+    x = x.strip()
+    if x[0] != '[':
+        x = f'[{x}'
+    if x[-1] != ']':
+        x =  f'{x}]'
+    return x
+
 
 # Create your views here.
 def index(request):
@@ -14,10 +37,12 @@ def index(request):
     load_dotenv()
 
     while True:
-        r = requests.get(f'https://api.github.com/repos/bhermann/DoR/issues/comments?page={page}&per_page=100', data={
+        session = requests_cache.CachedSession('dor-cache')
+        r = session.get(f'https://api.github.com/repos/bhermann/DoR/issues/comments?page={page}&per_page=100', data={
             'Authorization': 'token ' + os.getenv('TOKEN')
         })
         if r.status_code != 200:
+            print(r.content)
             return render(request, 'error.html', {'status': r.status_code})
 
         cur_comments = r.json()
@@ -33,10 +58,12 @@ def index(request):
     groups = [(key.split('/')[-1], list(data)) for key, data in groups]
 
     final_groups = []
+    scores_only = []
 
     # Process each issue
     for key, data in groups:
-        data = [x for x in data if 'reused:' in x['body']]
+        # Filter data
+        data = [x for x in data if 'reused:' in x['body'] or re.match(FILE_REGEX, x['body'])]
 
         # If there are no comments about reuse, skip
         if len(data) == 0:
@@ -51,13 +78,55 @@ def index(request):
         for comment in data:
             body = comment['body']
 
+            # Check for file in the comment.
+            link = re.match(FILE_REGEX, body)
+            if link is not None:
+                # We have a comment with a CSV
+                url = link.groups()[0]
+
+                # Get the file
+                with requests.get(url) as r:
+                    r.raise_for_status()
+                    df: pd.DataFrame = pd.read_csv(StringIO(r.content.decode('utf-8')))
+                    df['paper_doi'] = [x.strip() for x in df['paper_doi']]
+
+                    groups = df.groupby('paper_doi')
+                    for doi, group in groups:
+                        if doi not in cur_groups:
+                            # Build the DataFrame index
+                            index = group['citation_number']
+                            index = set([normalize_index(x) for x in index])
+
+                            cur_groups[doi] = pd.DataFrame(
+                                index=index, columns=['y', 'n'])
+                        
+                        for artifact in group['citation_number']:
+                            artifact = normalize_index(artifact)
+                            if artifact not in cur_groups[doi].index:
+                                # Seed the index
+                                cur_groups[doi].loc[artifact, 'y'] = (2 - multiple_comments)
+                                cur_groups[doi].loc[artifact,
+                                                        'n'] = len(data) - 1
+                            
+                            # Populate the DataFrame
+                            if cur_groups[doi].loc[artifact, :].sum() != len(data):
+                                cur_groups[doi].loc[artifact, 'y'] = (2 - multiple_comments)
+                                cur_groups[doi].loc[artifact,
+                                                        'n'] = len(data) - 1
+                            else:
+                                cur_groups[doi].loc[artifact, 'y'] += (2 - multiple_comments)
+                                cur_groups[doi].loc[artifact, 'n'] -= (2 - multiple_comments)
+
+                continue
+            elif 'reused:' not in body:
+                continue
             # Process each document
             for line in body.split('\n'):
                 if 'reused' in line:
                     # Mine the document and the reused artifacts
-                    document = line.split('reused:')[0].split()[-1]
+                    document = line.split('reused:')[0].split()[-1].strip()
                     reused = line.split('reused:')[1].split(',')
-                    reused = [x.strip() for x in reused]
+                    reused = [normalize_index(x) for x in reused]
 
                     # If document not in groups, add it
                     if document not in cur_groups:
@@ -66,6 +135,7 @@ def index(request):
 
                     # Process each artifact
                     for artifact in reused:
+                        artifact = normalize_index(artifact)
                         if artifact not in cur_groups[document].index:
                             # Seed the index
                             cur_groups[document].loc[artifact, 'y'] = (2 - multiple_comments)
@@ -82,12 +152,26 @@ def index(request):
                             cur_groups[document].loc[artifact, 'n'] -= (2 - multiple_comments)
 
         try:
-            kappas = []
-            for k, df in cur_groups.items():
-                kappa = round(fleiss_kappa(df.to_numpy(), 'uniform'), 2)
-                kappas.append((k, kappa, kappa < 0.6))
-            final_groups.append((key, kappas))
+            if len(cur_groups) > 0:
+                kappas = []
+                for k, df in cur_groups.items():
+                    kappa = round(fleiss_kappa(df.to_numpy(), 'uniform'), 2)
+                    scores_only.append(kappa)
+                    kappas.append((k, kappa, kappa < 0.6))
+                final_groups.append((key, kappas))
         except:
             pass
 
-    return render(request, "index.html", {'groups': final_groups})
+    _, ax = plt.subplots()
+    ax.hist(scores_only, density=True)
+    ax.set_xlabel('Fleiss\' Kappa')
+    ax.set_ylabel('Density')
+    ax.set_title('Distribution of Kappa scores')
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='jpg')
+    buf.seek(0)
+    base64_data = base64.b64encode(buf.read())
+    base64_data = 'data:image/jpg;base64,' + base64_data.decode('utf-8')
+
+    return render(request, "index.html", {'groups': final_groups, 'hist': base64_data})
