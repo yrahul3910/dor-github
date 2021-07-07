ecb116a8fed2810e053af5b09f33a40051bf7920
Rahul Yedida <rahul.yedida@pm.me>
2021-07-07 00:36:39 -0400

    Remove cache on startup

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # On startup, delete cache.
+    if os.path.exists('dor-cache.sqlite'):
+        os.remove('dor-cache.sqlite')
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
     from django.core.management import execute_from_command_line
