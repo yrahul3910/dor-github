005d60ded5e0f13190ce664f41b630a37b3cde7c
Rahul Yedida <rahul.yedida@pm.me>
2021-07-20 02:29:08 -0400

    Added more statistics, cache for CSVs, edge cases

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --timeout 120
+web: gunicorn gettingstarted.wsgi --timeout 240
--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import os
 from dotenv import load_dotenv
 from itertools import groupby
-from pandas.core.algorithms import isin
-from pandas.core.indexes import multi
-import requests
 from statsmodels.stats.inter_rater import fleiss_kappa
 import requests_cache
 import matplotlib.pyplot as plt
@@ -14,15 +10,40 @@ import pandas as pd
 import math
 import base64
 import io
+import statistics
 from io import StringIO
 import matplotlib as mpl
 import re
 from pprint import pprint
+from enum import Enum
+import time
+
+
+class IgnoreDirectives(Enum):
+    """
+    Specifies a set of comment parts that we should ignore.
+    """
+    LINKS = 1
+    TIMES = 2
+
+
+# Are we debugging?
+DEBUG = False
 
 FILE_REGEX = '\[.*\]\((https:\/\/github.com\/bhermann\/DoR\/files\/.*)\)'
+
+# Regex for time. Matches the formats:
+# 24:09
+# 24m 5s --> does not match the "s", but we do not need it anyway
+TIME_REGEX = '\d+m?(?:\s+)?:\d+'
+
 sns.set()
 
 def normalize_index(x):
+    """
+    Normalizes a citation. Adds in square brackets on each side
+    and removes spacing on the sides. Also converts to a str.
+    """
     if not isinstance(x, str):
         if not math.isnan(x):
             x = str(int(x))
@@ -39,14 +60,26 @@ def normalize_index(x):
     return x
 
 
+def normalize_doi(x):
+    """
+    Normalizes the DOIs to a URL.
+    """
+    x = x.strip()
+    if not x.startswith('http'):
+        x = f'https://doi.org/{x}'
+    
+    return x
+
+
 # Create your views here.
 def index(request):
     comments = []
     page = 1
     load_dotenv()
 
+    start = time.time()
+
     while True:
-        print('Fetching page', page)
         session = requests_cache.CachedSession('dor-cache', expire_after=3600)
         r = session.get(f'https://api.github.com/repos/bhermann/DoR/issues/comments?page={page}&per_page=100', data={
             'Authorization': 'token ' + os.getenv('TOKEN')
@@ -62,21 +95,35 @@ def index(request):
         
         comments.extend(cur_comments)
     
-    print('Processing', len(comments), 'comments.')
+    if DEBUG:
+        print('Processing', len(comments), 'comments.', round(time.time() - start, 2), 'seconds elapsed.')
 
     # Group comments by issue
     comments.sort(key=lambda p: p['issue_url'])
     groups = groupby(comments, key=lambda p: p['issue_url'])
     groups = [(key.split('/')[-1], list(data)) for key, data in groups]
 
+    # For kappa stats
     final_groups = []
     scores_only = []
+
+    # How long does it take to read papers?
+    reading_times = []
+
+    # How many issues do we have?
+    num_issues = len(groups)
+
+    # The other stats
     num_papers = {
         'multiple_reviewers': 0,
         'all_papers': 0,
         'kappa_1': 0,
         'good_kappa': 0
     }
+
+    # For debug purposes
+    issues_parsing_start_time = time.time()
+    issues_parsing_times = []
 
     # Process each issue
     for key, data in groups:
@@ -96,107 +143,165 @@ def index(request):
         for comment in data:
             body = comment['body']
 
-            # Check for file in the comment.
-            link = re.findall(FILE_REGEX, body)
-            if len(link) > 0:
-                # We have a comment with a CSV
-                url = link[0]
+            # Should we be ignoring anything?
+            ignore_directives = []
+            if body.startswith('#ignore'):
+                # What should we ignore?
+                first_line = body.split('\n')[0].split()[1:]
+                for item in first_line:
+                    if item == 'links':
+                        ignore_directives.append(IgnoreDirectives.LINKS)
+                    if item == 'times':
+                        ignore_directives.append(IgnoreDirectives.TIMES)
 
-                # Get the file
-                with requests.get(url) as r:
-                    r.raise_for_status()
+            # Parse times (if we should)
+            if IgnoreDirectives.TIMES not in ignore_directives:
+                # Get lines with the times
+                comment_lines = body.split('\n')
+                comment_lines = [x for x in comment_lines if ' took' in x]
 
-                    try:
-                        df: pd.DataFrame = pd.read_csv(StringIO(r.content.decode('latin-1')), sep=None, engine='python')
-                    except pd.errors.ParserError as err:
-                        print('Parse error in issue', key)
-                        print()
-                        continue
+                # Grab those times
+                for line in comment_lines:
+                    # Look for times
+                    intervals = re.findall(TIME_REGEX, line)
+                    if len(intervals) != 0:
+                        interval = intervals[0]
+                        if ' ' in interval:
+                            # We're processing a case of the format "24m 5s"
+                            interval_split = interval.split()
 
-                    df.columns = [x.strip() for x in df.columns]
+                            try:
+                                minutes = float(interval_split[0][:-1])
+                                minutes += float(interval_split[1]) / 60
+                                reading_times.append(minutes)
+                            except ValueError as err:
+                                print('Parsing error occurred for timestamp:', err)
+                                continue
+                        else:
+                            # We're parsing a case of the format mm:ss
+                            interval_split = interval.split(':')
 
-                    try:
-                        df.dropna(axis=0, inplace=True, subset=['paper_doi'])
-                    except KeyError as err:
-                        print(err, df.columns)
-                        
-                    try:
-                        df['paper_doi'] = [x.strip() for x in df['paper_doi']]
-                    except KeyError as err:
-                        print('In issue', key, 'column paper_doi not found. Columns are', df.columns)
-                        print()
-                        continue
-                    except AttributeError as err:
-                        print(err, df['paper_doi'])
-                        continue
+                            try:
+                                minutes = float(interval_split[0])
+                                minutes += float(interval_split[1]) / 60
+                                reading_times.append(minutes)
+                            except ValueError as err:
+                                print('Parsing error occurred for timestamp:', err)
+                                continue
 
-                    groups = df.groupby('paper_doi')
-                    for doi, group in groups:
-                        if doi not in cur_groups:
-                            # Build the DataFrame index
-                            index = group['citation_number']
-                            index = set([normalize_index(x) for x in index])
+            # Make sure we want to parse links
+            if IgnoreDirectives.LINKS not in ignore_directives:
+                # Check for file in the comment.
+                link = re.findall(FILE_REGEX, body)
+                if len(link) > 0:
+                    # We have a comment with a CSV
+                    url = link[0]
 
-                            cur_groups[doi] = pd.DataFrame(
-                                index=index, columns=['y', 'n'])
-                        
-                        for artifact in group['citation_number']:
-                            artifact = normalize_index(artifact)
-                            if artifact not in cur_groups[doi].index:
-                                # Seed the index
-                                cur_groups[doi].loc[artifact, 'y'] = (2 - multiple_comments)
-                                cur_groups[doi].loc[artifact,
-                                                        'n'] = len(data) - 1
+                    # Get the file
+                    session = requests_cache.CachedSession('dor-cache-files', expire_after=3600*24*7)
+                    with session.get(url) as r:
+                        r.raise_for_status()
+
+                        # Get the data
+                        try:
+                            df: pd.DataFrame = pd.read_csv(StringIO(r.content.decode('latin-1')), sep=None, engine='python')
+                        except pd.errors.ParserError as err:
+                            print('Parse error in issue', key, ':', err)
+                            print()
+                            continue
+
+                        # Preprocess the columns
+                        df.columns = [x.strip() for x in df.columns]
+
+                        # Dump rows with no paper_doi (reusing DOI)
+                        try:
+                            df.dropna(axis=0, inplace=True, subset=['paper_doi'])
+                        except KeyError as err:
+                            print(err, df.columns)
+
+                        # Normalize the DOIs  
+                        try:
+                            df['paper_doi'] = [normalize_doi(x) for x in df['paper_doi']]
+                        except KeyError as err:
+                            print('In issue', key, 'column paper_doi not found. Columns are', df.columns)
+                            print()
+                            continue
+                        except AttributeError as err:
+                            print(err, df['paper_doi'])
+                            continue
+
+                        # For each paper, check agreement
+                        groups = df.groupby('paper_doi')
+                        for doi, group in groups:
+                            if doi not in cur_groups:
+                                # Build the DataFrame index
+                                index = group['citation_number']
+                                index = set([normalize_index(x) for x in index])
+
+                                cur_groups[doi] = pd.DataFrame(
+                                    index=index, columns=['y', 'n'])
                             
+                            for artifact in group['citation_number']:
+                                artifact = normalize_index(artifact)
+                                if artifact not in cur_groups[doi].index:
+                                    # Seed the index
+                                    cur_groups[doi].loc[artifact, 'y'] = (2 - multiple_comments)
+                                    cur_groups[doi].loc[artifact,
+                                                            'n'] = len(data) - 1
+                                
+                                # Populate the DataFrame
+                                if cur_groups[doi].loc[artifact, :].sum() != len(data):
+                                    cur_groups[doi].loc[artifact, 'y'] = (2 - multiple_comments)
+                                    cur_groups[doi].loc[artifact,
+                                                            'n'] = len(data) - 1
+                                else:
+                                    cur_groups[doi].loc[artifact, 'y'] += (2 - multiple_comments)
+                                    cur_groups[doi].loc[artifact, 'n'] -= (2 - multiple_comments)
+
+                    continue
+                elif 'reused:' not in body:
+                    continue
+                # Process each document
+                for line in body.split('\n'):
+                    if 'reused' in line:
+                        # Mine the document and the reused artifacts
+                        document = line.split('reused:')[0].split()[-1].strip()
+                        reused = line.split('reused:')[1].split(',')
+                        reused = [normalize_index(x) for x in reused]
+
+                        # If document not in groups, add it
+                        if document not in cur_groups:
+                            cur_groups[document] = pd.DataFrame(
+                                index=reused, columns=['y', 'n'])
+
+                        # Process each artifact
+                        for artifact in reused:
+                            artifact = normalize_index(artifact)
+                            if artifact not in cur_groups[document].index:
+                                # Seed the index
+                                cur_groups[document].loc[artifact, 'y'] = (2 - multiple_comments)
+                                cur_groups[document].loc[artifact,
+                                                        'n'] = len(data) - 1
+
                             # Populate the DataFrame
-                            if cur_groups[doi].loc[artifact, :].sum() != len(data):
-                                cur_groups[doi].loc[artifact, 'y'] = (2 - multiple_comments)
-                                cur_groups[doi].loc[artifact,
+                            if cur_groups[document].loc[artifact, :].sum() != len(data):
+                                cur_groups[document].loc[artifact, 'y'] = (2 - multiple_comments)
+                                cur_groups[document].loc[artifact,
                                                         'n'] = len(data) - 1
                             else:
-                                cur_groups[doi].loc[artifact, 'y'] += (2 - multiple_comments)
-                                cur_groups[doi].loc[artifact, 'n'] -= (2 - multiple_comments)
-
-                continue
-            elif 'reused:' not in body:
-                continue
-            # Process each document
-            for line in body.split('\n'):
-                if 'reused' in line:
-                    # Mine the document and the reused artifacts
-                    document = line.split('reused:')[0].split()[-1].strip()
-                    reused = line.split('reused:')[1].split(',')
-                    reused = [normalize_index(x) for x in reused]
-
-                    # If document not in groups, add it
-                    if document not in cur_groups:
-                        cur_groups[document] = pd.DataFrame(
-                            index=reused, columns=['y', 'n'])
-
-                    # Process each artifact
-                    for artifact in reused:
-                        artifact = normalize_index(artifact)
-                        if artifact not in cur_groups[document].index:
-                            # Seed the index
-                            cur_groups[document].loc[artifact, 'y'] = (2 - multiple_comments)
-                            cur_groups[document].loc[artifact,
-                                                     'n'] = len(data) - 1
-
-                        # Populate the DataFrame
-                        if cur_groups[document].loc[artifact, :].sum() != len(data):
-                            cur_groups[document].loc[artifact, 'y'] = (2 - multiple_comments)
-                            cur_groups[document].loc[artifact,
-                                                     'n'] = len(data) - 1
-                        else:
-                            cur_groups[document].loc[artifact, 'y'] += (2 - multiple_comments)
-                            cur_groups[document].loc[artifact, 'n'] -= (2 - multiple_comments)
+                                cur_groups[document].loc[artifact, 'y'] += (2 - multiple_comments)
+                                cur_groups[document].loc[artifact, 'n'] -= (2 - multiple_comments)
 
         try:
+            # Do we have kappas in the issue?
             if len(cur_groups) > 0:
                 kappas = []
                 for k, df in cur_groups.items():
+                    # Workaround for issue with kappa > 1
                     kappa = min(1., round(fleiss_kappa(df.to_numpy(), 'uniform'), 2))
                     kappas.append((k, kappa))
+
+                    # Collect stats
                     num_papers['all_papers'] += 1
 
                     if multiple_comments:
@@ -212,16 +317,27 @@ def index(request):
         except:
             pass
 
-    _, ax = plt.subplots()
-    sns.histplot(data=scores_only, alpha=0.7, kde=True,  ax=ax)
-    ax.set_xlabel('Fleiss\' Kappa')
-    ax.set_ylabel('Density')
-    ax.set_title('Distribution of Kappa scores')
+        # Record the time taken to process the issue
+        issues_parsing_times.append((key, len(data), round(time.time() - issues_parsing_start_time, 2)))
+        issues_parsing_start_time = time.time()
+
+    _, [ax0, ax1] = plt.subplots(nrows=2, ncols=1, figsize=(6, 8), tight_layout=True)
+
+    sns.histplot(data=scores_only, alpha=0.7, kde=True,  ax=ax0)
+    ax0.set_xlabel('Fleiss\' Kappa')
+    ax0.set_ylabel('Frequency')
+    ax0.set_title('Distribution of Kappa scores')
 
     # Plot the minor gridlines too
-    ax.get_xaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
-    ax.grid(b=True, which='major', color='w', linewidth=1.0)
-    ax.grid(b=True, which='minor', color='w', linewidth=0.5)
+    ax0.get_xaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
+    ax0.grid(b=True, which='major', color='w', linewidth=1.0)
+    ax0.grid(b=True, which='minor', color='w', linewidth=0.5)
+
+    # Now plot times
+    sns.histplot(data=reading_times, alpha=0.7, kde=True, ax=ax1)
+    ax1.set_xlabel('Reading time (min)')
+    ax1.set_ylabel('Frequency')
+    ax1.set_title('Distribution of reading times')
 
     buf = io.BytesIO()
     plt.savefig(buf, format='jpg')
@@ -229,4 +345,17 @@ def index(request):
     base64_data = base64.b64encode(buf.read())
     base64_data = 'data:image/jpg;base64,' + base64_data.decode('utf-8')
 
-    return render(request, "index.html", {'num_papers': num_papers, 'groups': final_groups, 'hist': base64_data})
+    # Print the issue parsing times
+    if DEBUG:
+        print('Total time elapsed:', time.time() - start, 'seconds')
+        pprint(issues_parsing_times)
+
+    return render(request, "index.html",
+        {
+            'num_papers': num_papers,
+            'num_issues': num_issues,
+            'groups': final_groups,
+            'hist': base64_data,
+            'median_read_time': round(statistics.median(reading_times), 2)
+        }
+    )
